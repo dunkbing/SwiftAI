bc87def6f96a77c4dae8e9093e866cfe5eabe3d8
Binh.BuiDang <dangbinh4869@gmail.com>
2025-05-04 21:53:57 +0700

    update platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftAI",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -16,6 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftAI"),
+        .executableTarget(
+            name: "Examples",
+            dependencies: [
+                "SwiftAI"
+            ],
+            path: "Sources/Examples"),
         .testTarget(
             name: "SwiftAITests",
             dependencies: ["SwiftAI"]),
